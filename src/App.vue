802e118ca257d2289f19a30e4d76000cc163781a
Nir Tamir <nirtamir2@gmail.com>
2018-07-09 15:38:04 +0300

    create question component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <question :question="question"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Question from './components/Question.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Question
+  },
+  data() {
+    return {
+      question: {}
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Question.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>{{question}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Question',
+  props: {
+    question: {type: Object, required: true}
+  }
+}
+</script>
+
+<style scoped>
+</style>
